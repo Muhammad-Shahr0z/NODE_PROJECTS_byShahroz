8dd58011540d5c8b98e17303abb45bc7bc2ce154
Muhammad-Shahr0zMuhammad-Shahr0z <shahroxyt@gmail.com>
2024-03-22 21:00:58 +0500

    first commit

---
+++ project00_Simple-Calculator/index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer: any = await inquirer.prompt([
+  { message: "Enter Your First Number", type: "number", name: "firstNumber" }, //Name is a Variable that Store Number
+  { message: "Enter Your Second Number", type: "number", name: "secondNumber" }, //Name is a Variable that Store Number
+  {
+    message: "Choose One Operator To Perform Operation",
+    type: "list",
+    name: "Operator",
+    choices: ["Division", "Multiplication", "Addition", "Subtraction"],
+  },
+]);
+
+if (answer.Operator === "Division") {
+  console.log(`Result is ${answer.firstNumber / answer.secondNumber} `);
+} else if (answer.Operator === "Multiplication") {
+  console.log(`Result is ${answer.firstNumber * answer.secondNumber} `);
+} else if (answer.Operator === "Addition") {
+  console.log(`Result is ${answer.firstNumber + answer.secondNumber} `);
+} else if (answer.Operator === "Subtraction") {
+  console.log(`Result is ${answer.firstNumber - answer.secondNumber} `);
+} else {
+  console.log("Pleas Enter Valid Number or Oprator ");
+}
